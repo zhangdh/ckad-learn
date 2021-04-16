1431e917da4d781f78aad1563e599e2005d8cf71
zhangdh <zhangdonghai1989@126.com>
2021-04-16 14:56:13 +0800

    Update sh2.sh

--- sh2.sh
+++ sh2.sh
@@ -1,7 +1,8 @@
 while true;
   do random = $(((RADOM%100)+1));
-  if [ $random -le 50 ];
-  then echo "$random";
-  else echo "END: $random"; break;fi;
-  sleep 1;
+  if [ $random -le 50 ]; then 
+    echo "$random";
+  else 
+    echo "END: $random"; break;fi;
+    sleep 1;
   done;
